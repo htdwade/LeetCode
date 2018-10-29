2b5290c82e792753d32b206fb58c3d4a7b79f0d5
htdwade <1046217644@qq.com>
2018-10-29 15:36:12 +0800

    add 406

---
+++ 406.QueueReconstructionByHeight/QueueReconstructionByHeight.cpp
@@ -0,0 +1,50 @@
+/*
+
+Suppose you have a random list of people standing in a queue. Each person is described by a pair of integers (h, k),
+where h is the height of the person and k is the number of people in front of this person who have a height greater 
+than or equal to h. Write an algorithm to reconstruct the queue.
+
+Note:
+The number of people is less than 1,100.
+
+
+Example
+
+Input:
+[[7,0], [4,4], [7,1], [5,0], [6,1], [5,2]]
+
+Output:
+[[5,0], [7,0], [5,2], [6,1], [4,4], [7,1]]
+
+*/
+
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+	vector<pair<int, int>> reconstructQueue(vector<pair<int, int>>& people) {
+		auto cmp = [](const pair<int, int>& p1, const pair<int, int>& p2)
+		{return p1.first > p2.first || (p1.first == p2.first && p1.second < p2.second); };
+		sort(people.begin(), people.end(), cmp);
+		vector<pair<int, int>> res;
+		for (auto p : people)
+			res.insert(res.begin() + p.second, p);
+		return res;
+	}
+};
+
+int main()
+{
+	Solution solution;
+	vector<pair<int, int>> people = { {7,0},{4,4},{7,1},{5,0},{6,1},{5,2} };
+	vector<pair<int, int>> res = solution.reconstructQueue(people);
+	for (auto s : res) {
+		cout << s.first << '\t' << s.second << endl;
+	}
+	return 0;
+}
