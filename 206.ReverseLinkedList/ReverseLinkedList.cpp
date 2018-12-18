6b6f5dc0ed3198795a377464c5df8e2b70498fa1
htdwade <1046217644@qq.com>
2018-12-18 17:31:38 +0800

    updated

--- 206.ReverseLinkedList/ReverseLinkedList.cpp
+++ 206.ReverseLinkedList/ReverseLinkedList.cpp
@@ -29,14 +29,34 @@ public:
 		ListNode* pre = nullptr;
 		ListNode* cur = head;
 		while (cur) {
-			ListNode* next = cur->next;
+			ListNode* pNext = cur->next;
 			cur->next = pre;
 			pre = cur;
-			cur = next;
+			cur = pNext;
 		}
 		return pre;
 	}
+
+	//递归
+	ListNode* reverseList2(ListNode* head) {
+		if (head == nullptr || head->next == nullptr)
+			return head;
+		ListNode* pNext = head->next;
+		ListNode* newHead = reverseList(pNext);
+		pNext->next = head;
+		head->next = nullptr;
+		return newHead;
+	}
 };
+
+void printList(ListNode* head) {
+	ListNode* p = head;
+	while (p) {
+		cout << p->val << '\t';
+		p = p->next;
+	}
+	cout << '\n';
+}
 
 int main()
 {
@@ -46,15 +66,13 @@ int main()
 	ListNode* p3 = new ListNode(3);
 	ListNode* p4 = new ListNode(4);
 	ListNode* p5 = new ListNode(5);
-	ListNode* head = p1;
 	p1->next = p2;
 	p2->next = p3;
 	p3->next = p4;
 	p4->next = p5;
-	ListNode* p = solution.reverseList(head);
-	while (p) {
-		cout << p->val << '\t';
-		p = p->next;
-	}
+	ListNode* head = p1;
+	printList(head);
+	ListNode* newHead = solution.reverseList(head);
+	printList(newHead);
 	return 0;
 }
