a262b41b33a62cf8f6654e9fcbae36c510345f7a
htdwade <1046217644@qq.com>
2020-03-19 16:44:32 +0800

    add 633

--- 0633.SumOfSquareNumbers/SumOfSquareNumbers.cpp
+++ 0633.SumOfSquareNumbers/SumOfSquareNumbers.cpp
@@ -1,45 +1,40 @@
 /*
 
-Given a non-negative integer c, your task is to decide whether there're two integers a and b such that a2 + b2 = c.
+给定一个非负整数 c ，你要判断是否存在两个整数 a 和 b，使得 a2 + b2 = c。
 
-Example 1:
-Input: 5
-Output: True
-Explanation: 1 * 1 + 2 * 2 = 5
+示例1:
 
-Example 2:
-Input: 3
-Output: False
+输入: 5
+输出: True
+解释: 1 * 1 + 2 * 2 = 5
+ 
+
+示例2:
+
+输入: 3
+输出: False
 
 */
 
 
-#include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 class Solution {
 public:
-	bool judgeSquareSum(int c) {
-		if (c < 0) return false;
-		int i = 0, j = (int)sqrt(c);
-		while (i <= j) {
-			int sum = i * i + j * j;
-			if (sum == c)
+    bool judgeSquareSum(int c) {
+        if (c < 0) 
+			return false;
+		int left = 0, right = (int)sqrt(c);
+		while (left <= right) {
+			if (left * left == c - right * right)
 				return true;
-			else if (sum < c)
-				i++;
+			else if (left * left < c - right * right)
+				left++;
 			else
-				j--;
+				right--;
 		}
 		return false;
-	}
+    }
 };
-
-int main()
-{
-	Solution solution;
-	cout << solution.judgeSquareSum(5) << endl;
-	cout << solution.judgeSquareSum(3) << endl;
-	return 0;
-}
