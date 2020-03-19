dd5347e2f3957f6cb00129a481c35a5fea1dacef
htdwade <1046217644@qq.com>
2020-03-19 16:47:35 +0800

    update 633

--- 0633.SumOfSquareNumbers/SumOfSquareNumbers.java
+++ 0633.SumOfSquareNumbers/SumOfSquareNumbers.java
@@ -18,18 +18,18 @@
 
 class Solution {
     public boolean judgeSquareSum(int c) {
-        if (c < 0) 
-			return false;
-		int left = 0, right = (int)Math.sqrt(c);
-		while (left <= right) {
-			int sum = left * left + right * right;
-			if (sum == c)
-				return true;
-			else if (sum < c)
-				left++;
-			else
-				right--;
-		}
-		return false;
+        if (c < 0)
+            return false;
+        int left = 0, right = (int) Math.sqrt(c);
+        while (left <= right) {
+            int sum = left * left + right * right;
+            if (sum == c)
+                return true;
+            else if (sum < c)
+                left++;
+            else
+                right--;
+        }
+        return false;
     }
 }
